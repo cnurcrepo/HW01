c945b360ea6e9d8349310d589d4316767108dc33
lsh23 <shseoul14@gmail.com>
2019-04-14 16:58:25 +0900

    Edit RiotService.java refactoring & Add throws exception

--- call-Summoner/src/main/java/org/cnu/realcoding/callSummoner/callSummoner/service/RiotService.java
+++ call-Summoner/src/main/java/org/cnu/realcoding/callSummoner/callSummoner/service/RiotService.java
@@ -1,9 +1,12 @@
 package org.cnu.realcoding.callSummoner.callSummoner.service;
 
 import org.cnu.realcoding.callSummoner.callSummoner.api.SummonerApiClient;
+import org.cnu.realcoding.callSummoner.callSummoner.domain.League;
 import org.cnu.realcoding.callSummoner.callSummoner.repository.RiotDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class RiotService {
@@ -13,9 +16,9 @@ public class RiotService {
     @Autowired
     private SummonerApiClient summonerApiClient;
 
-    public void insertDataInMongoDB(String nameOfSummoner) {
-        String idOfSummoner = summonerApiClient.getLeague(nameOfSummoner).getID();
-        String leagueOfGammer = this.summonerApiClient.getLeagueInfoByID(idOfSummoner);
-        riotDataRepository.insertRoitData(leagueOfGammer);
+    public void insertDataInMongoDB(String nameOfSummoner) throws IOException {
+        String idOfSummoner = summonerApiClient.getDataOfEncrypedID(nameOfSummoner).getID();
+        League leagueOfGamer = this.summonerApiClient.getLeagueInfoByID(idOfSummoner);
+        riotDataRepository.insertRoitData(leagueOfGamer);
     }
 }
